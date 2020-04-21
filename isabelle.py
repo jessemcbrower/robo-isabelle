a1c214d2fd15a6f989702ca8247a78c369a093fc
Jesse Brower <jesse.brower@mullenlowe.com>
2020-04-21 19:30:44 -0400

    added turnip functions

--- isabelle.py
+++ isabelle.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import json
 import os
 import collections.abc
+import time
 
 client = commands.Bot(command_prefix = '!')
 os.chdir('/Users/jesse.brower/projects/robo-isabelle')
@@ -46,9 +47,47 @@ async def add_user(users, user):
 
         users[f'{user.id}'] = {}
         users[f'{user.id}']['username'] = str(user)
+        users[f'{user.id}']['turnips'] = "n/a"
+        users[f'{user.id}']['time'] = "n/a"
 
 async def update_code(users, user, code):
 
     users[f'{user.id}']['code'] = str(code)
+
+@client.command()
+async def myPrice(ctx, arg):
+
+    with open('users.json', 'r') as d:
+        users = json.load(d)
+
+    await add_user(users, ctx.author)
+    await update_turnips(users, ctx.author, arg)
+
+    with open('users.json', 'w') as d:
+        json.dump(users, d)
+
+    await ctx.send(f'{ctx.author} updated their turnips price to {arg}.')
+    print(f'{ctx.author} updated their turnips price to {arg}.')
+
+async def update_turnips(users, user, turnips):
+
+    users[f'{user.id}']['turnips'] = str(turnips)
+    users[f'{user.id}']['time'] = time.asctime(time.localtime(time.time()))
+
+@client.command()
+async def priceCheck(ctx):
+
+    with open('users.json', 'r') as d:
+        users = json.load(d)
+
+    embed = discord.Embed(title="Turnips Prices")
+
+    for user in users:
+        if users[f'{user}']['turnips'] != 'n/a':
+
+            embed.add_field(name=users[f'{user}']['username'], value=users[f'{user}']['turnips'], inline=False)
+            embed.add_field(name='Time Stamp:', value=users[f'{user}']['time'], inline=False)
+
+    await ctx.send(embed=embed)
 
 client.run(Your_Token)
